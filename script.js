07f5e9d7829d0abb53634d575a803a7d449699ff
Zane Marshall <zamarshall24@gmail.com>
2022-03-31 21:11:03 -0500

    added sugar function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var requestUrl =
-  "https://api.nal.usda.gov/fdc/v1/foods/search?query=snickers&pageSize=2&api_key=vuZ8WUcvpMr1mNoGUwWsyX4AWHv3LLaeRcZpDoga";
+  "https://api.nal.usda.gov/fdc/v1/foods/search?query=twix&pageSize=2&api_key=vuZ8WUcvpMr1mNoGUwWsyX4AWHv3LLaeRcZpDoga";
 
 function getRequest() {
   fetch(requestUrl)
@@ -8,20 +8,27 @@ function getRequest() {
       console.log(data);
       console.log(data.foods[0].foodNutrients);
       var foodNutrients = data.foods[0].foodNutrients;
-
-      var calories = foodNutrients.find(function (nutrient){
-       // console.log(nutrient.nutrientName);
-    //    return nutrient.nutrientName==="Energy";
-        if (nutrient.nutrientName==="Energy"){
-            return true
-        }else{
-            return false
+      var sugar = foodNutrients.find(function(nutrient){
+        if(nutrient.nutrientName === "Sugars, total including NLEA") {
+          return true;
+        } else {
+          return false;
         }
 
+      });
 
-      })
+      var calories = foodNutrients.find(function (nutrient) {
+        // console.log(nutrient.nutrientName);
+        //    return nutrient.nutrientName==="Energy";
+        if (nutrient.nutrientName === "Energy") {
+          return true;
+        } else {
+          return false;
+        }
+      });
 
       console.log(calories.value);
+      console.log(sugar.value)
     });
 }
-getRequest(); 
+getRequest();
