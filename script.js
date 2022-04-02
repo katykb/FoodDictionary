9fdb78730824d68c0721727b0cf87d40bb31cbbe
J Clancy <ukg_123@yahoo.com>
2022-04-02 11:11:12 -0500

    added foodName, protein, and .unitName in console

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-var requestUrl =
-    "https://api.nal.usda.gov/fdc/v1/foods/search?query=twix&pageSize=2&api_key=vuZ8WUcvpMr1mNoGUwWsyX4AWHv3LLaeRcZpDoga";
+function getRequest(foodName) {
+    var requestUrl =
+        "https://api.nal.usda.gov/fdc/v1/foods/search?query=" +
+        foodName +
+        "&pageSize=2&api_key=vuZ8WUcvpMr1mNoGUwWsyX4AWHv3LLaeRcZpDoga";
 
-function getRequest() {
     fetch(requestUrl)
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
             console.log(data.foods[0].foodNutrients);
             var foodNutrients = data.foods[0].foodNutrients;
+
             var sugar = foodNutrients.find(function(nutrient) {
                 if (nutrient.nutrientName === "Sugars, total including NLEA") {
                     return true;
                 } else {
                     return false;
                 }
-
             });
 
             var calories = foodNutrients.find(function(nutrient) {
@@ -27,8 +29,13 @@ function getRequest() {
                 }
             });
 
-            console.log(calories.value);
-            console.log(sugar.value)
+            var protein = foodNutrients.find(function(nutrient) {
+                return nutrient.nutrientName === "Protein"; //same as above just written different.
+            });
+
+            console.log(calories.value + " " + calories.unitName);
+            console.log(sugar.value);
+            console.log(protein.value + " " + protein.unitName);
         });
 }
-getRequest();
+getRequest("chicken");
